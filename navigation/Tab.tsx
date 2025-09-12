ac248d75bac251f0e381b4f2b311472733b94110
Santhu32144 <ssshininnsanthu32144@gmail.com>
2025-09-12 18:03:54 +0530

    initial commit

---
+++ navigation/Tab.tsx
@@ -0,0 +1,48 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import React from 'react';
+
+const Tab = createBottomTabNavigator();
+import Home from '../screens/Home';
+import Profile from '../screens/Profile';
+import Details from '../screens/Details';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {useColorScheme} from 'react-native';
+
+const TabNavigator = () => {
+  const colorScheme = useColorScheme();
+  const isDark = colorScheme === 'dark';
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: isDark ? '#FFF' : '#000',
+                tabBarInactiveTintColor: isDark ? '#AAA' : '#555',
+            }}
+        >
+            <Tab.Screen
+                name="Home"
+                component={Home}
+                options={{
+                    tabBarIcon: ({color}) => <Icon name="home" size={20} color={color} />,
+                }}
+            />
+            <Tab.Screen
+                name="Profile"
+                component={Profile}
+                options={{
+                    tabBarIcon: ({color}) => <Icon name="user" size={20} color={color} />,
+                }}
+            />
+            <Tab.Screen
+                name="Details"
+                component={Details}
+                options={{
+                    tabBarIcon: ({color}) => <Icon name="info" size={20} color={color} />,
+                }}
+            />
+        </Tab.Navigator>
+    );
+};
+
+export default TabNavigator;
